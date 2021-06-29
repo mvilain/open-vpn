09f5628d1d4a827e684bc10a497c6f8ea59354a2
Michael Vilain <michael@vilain.com>
2021-06-29 15:31:49 -0700

    disable fail2ban

--- linode/linode-stackscripts.tf
+++ linode/linode-stackscripts.tf
@@ -22,7 +22,7 @@ resource "linode_stackscript" "rhel7" {
     system_set_hostname $HOSTNAME
     system_add_host_entry $(system_primary_ip) $HOSTNAME.$DOMAIN $HOSTNAME
     system_configure_ntp
-    enable_fail2ban
+    #enable_fail2ban
 
     yum install -y epel-release
     yum install -y python3 libselinux-python3 git
@@ -30,7 +30,7 @@ resource "linode_stackscript" "rhel7" {
   images = [
     "linode/centos7"
   ]
-  rev_note = "2021.06.27"
+  rev_note = "2021.06.29"
 }
 
 resource "linode_stackscript" "rhel8" {
@@ -48,7 +48,7 @@ resource "linode_stackscript" "rhel8" {
     system_set_hostname $HOSTNAME
     system_add_host_entry $(system_primary_ip) $HOSTNAME.$DOMAIN $HOSTNAME
     system_configure_ntp
-    enable_fail2ban
+    #enable_fail2ban
 
     dnf config-manager --set-enabled powertools
     dnf makecache
@@ -60,7 +60,7 @@ resource "linode_stackscript" "rhel8" {
     "linode/centos8",
     "linode/rocky8"
   ]
-  rev_note = "2021.06.27"
+  rev_note = "2021.06.29"
 }
 
 resource "linode_stackscript" "debian" {
@@ -78,7 +78,7 @@ resource "linode_stackscript" "debian" {
     system_set_hostname $HOSTNAME
     system_add_host_entry $(system_primary_ip) $HOSTNAME.$DOMAIN $HOSTNAME
     system_configure_ntp
-    enable_fail2ban
+    #enable_fail2ban
     debian_upgrade
 
     apt-get install -y apt-transport-https python-apt
@@ -87,7 +87,7 @@ resource "linode_stackscript" "debian" {
     "linode/debian9", 
     "linode/debian10"
   ]
-  rev_note = "2021.06.27"
+  rev_note = "2021.06.29"
 }
 
 resource "linode_stackscript" "fedora" {
@@ -105,7 +105,7 @@ resource "linode_stackscript" "fedora" {
     system_set_hostname $HOSTNAME
     system_add_host_entry $(system_primary_ip) $HOSTNAME.$DOMAIN $HOSTNAME
     system_configure_ntp
-    enable_fail2ban
+    #enable_fail2ban
 
     dnf install -y python3
   EOF
@@ -114,7 +114,7 @@ resource "linode_stackscript" "fedora" {
     "linode/fedora33",
     "linode/fedora34"
   ]
-  rev_note = "2021.06.27"
+  rev_note = "2021.06.29"
 }
 
 resource "linode_stackscript" "ubuntu" {
@@ -132,7 +132,7 @@ resource "linode_stackscript" "ubuntu" {
     system_set_hostname $HOSTNAME
     system_add_host_entry $(system_primary_ip) $HOSTNAME.$DOMAIN $HOSTNAME
     system_configure_ntp
-    enable_fail2ban
+    #enable_fail2ban
 
     apt-get install -y install python3
   EOF
@@ -140,5 +140,5 @@ resource "linode_stackscript" "ubuntu" {
     "linode/ubuntu18.04",
     "linode/ubuntu20.04",
   ]
-  rev_note = "2021.06.27"
+  rev_note = "2021.06.29"
 }
