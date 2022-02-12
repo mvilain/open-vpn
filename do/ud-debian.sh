3b9d5df20e8c06270b1b984f339e53891522abe6
Michael Vilain <michael@vilain.com>
2022-02-11 21:06:14 -0800

    remove support for digital ocean

--- do/ud-debian.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "preserve_hostname: false" >> /etc/cloud/cloud.cfg
-
-apt-get update -y
-apt-get install -y apt-transport-https python-apt
--- do/ud-redhat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "preserve_hostname: false" >> /etc/cloud/cloud.cfg
-
-dnf install -y epel-release
-dnf config-manager --set-enabled powertools
-dnf makecache
-#    dnf install -y ansible
-alternatives --set python /usr/bin/python3
